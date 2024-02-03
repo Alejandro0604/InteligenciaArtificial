1f5fbb5918b2ed6e81c61f0b68c3024100ed6fd2
alejandro <kychavezso@gmail.com>
2024-02-03 11:32:37 -0600

    Busqueda ciega

---
+++ BusquedaCiega.py
@@ -0,0 +1,42 @@
+from collections import deque
+
+def bfs(grafico, inicio, objetivo):
+    #Utilizamos una cola para la búsqueda en amplitud
+    cola = deque([(inicio, [inicio])])
+
+    while cola:
+        nodo, camino = cola.popleft()
+
+        if nodo == objetivo:
+            return camino
+        
+        for vecino in grafico[nodo]:
+            if vecino not in camino:
+                cola.append((vecino, camino + [vecino]))
+
+    #Si no se encuentra el objetivo
+    return None
+
+
+grafo = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F', 'G'],
+    'D': ['B'],
+    'E': ['B', 'H'],
+    'F': ['C'],
+    'G': ['C'],
+    'H': ['E']
+}
+
+#inicio
+inicio = 'A'
+#objetivo
+objetivo = 'E'
+
+resultado = bfs(grafo, inicio, objetivo)
+
+if resultado:
+    print(f"Camino de {inicio} a {objetivo}: {resultado}")
+else:
+    print(f"No se encontró un camino de {inicio} a {objetivo}")
